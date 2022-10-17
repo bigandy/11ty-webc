d70d871e77f76d32e520e8e9acf160b6e9aeb000
Andrew Hudson <ahudson@gmail.com>
2022-10-17 20:11:03 +0100

    noodling about

--- .eleventy.js
+++ .eleventy.js
@@ -1,52 +1,6 @@
 const slinkity = require('slinkity');
 const { EleventyRenderPlugin } = require("@11ty/eleventy");
 const pluginWebc = require("@11ty/eleventy-plugin-webc");
-const { DateTime } = require('luxon');
-
-const getNumberSuffix = (num) => {
-	const th = 'th';
-	const rd = 'rd';
-	const nd = 'nd';
-	const st = 'st';
-
-	if (num === 11 || num === 12 || num === 13) return th;
-
-	const lastDigit = num.toString().slice(-1);
-
-	switch (lastDigit) {
-		case '1':
-			return 'st';
-		case '2':
-			return 'nd';
-		case '3':
-			return 'rd';
-		default:
-			return 'th';
-	}
-};
-
-const readableDate = (dateObj) => {
-	const formatDate = DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat(
-		'd-LLLL-yyyy'
-	);
-
-	const [day, month, year] = formatDate.split('-');
-	const outputDay = `${day}${getNumberSuffix(day)}`;
-
-	return `${outputDay} ${month} ${year}`;
-};
-
-const firstNElements = (array, n) => {
-	if (n < 0) {
-		return array.slice(n);
-	}
-
-	return array.slice(0, n);
-};
-
-const htmlDateString = dateObj => {
-	return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
-};
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyRenderPlugin);
@@ -56,23 +10,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(slinkity.plugin, slinkity.defineConfig({
     renderers: [],
   }));
-
-  eleventyConfig.addFilter('firstNElements', firstNElements);
-  eleventyConfig.addFilter('htmlDateString', htmlDateString);
-  eleventyConfig.addFilter('readableDate', readableDate);
-
-  readableDate
-
-  eleventyConfig.addCollection('posts', (collection) => {
-    const returnPostCollection = collection
-      .getFilteredByGlob('./src/posts/**/*.md');
-      // .filter(showWeeknotes)
-      // .filter(livePosts)
-      // .filter(removeDrafts);
-
-    return returnPostCollection;
-  });
-
 
   /**
    * Why copy the /public directory?
